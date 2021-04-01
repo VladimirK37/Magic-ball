c0125d8315dd4cf80a6d7cf71a4a8f55f2c4a1be
VladimirK37 <61294220+VladimirK37@users.noreply.github.com>
2021-04-01 15:57:24 +0300

    Add json

--- MagicBall/Form1.Designer.cs
+++ MagicBall/Form1.Designer.cs
@@ -91,6 +91,7 @@ namespace MagicBall
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.splitContainer1.Panel1.ResumeLayout(false);
             this.splitContainer1.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
--- MagicBall/Form1.cs
+++ MagicBall/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -13,6 +14,8 @@ namespace MagicBall
 {
     public partial class Form1 : Form
     {
+        private const string APP_Name = "MAGIC_BALL";
+        private readonly string PREDICTIONS_CONFIG_PATH = $"{Environment.CurrentDirectory}\\PredictionsConfig.json";
         public Form1()
         {
             InitializeComponent();
@@ -20,19 +23,26 @@ namespace MagicBall
 
         private async void bPredict_Click(object sender, EventArgs e)
         {
+            bPredict.Enabled = false;//отключение кнопки GO
             await Task.Run(() =>
             {
-                for (int i = 1; i < 100; i++)
+                for (int i = 1; i <= 100; i++)
                 {
                     Invoke(new Action(() =>
                     {
                         UpdateProgressBar(i);
+                        Text = $"{i}%";
                     }));
                     Thread.Sleep(50);
                 }
             });
 
             MessageBox.Show("message");
+
+            progressBar1.Value = 0;
+
+            Text = APP_Name;
+            bPredict.Enabled = true;//использование повторно кнопки GO
         }
 
         private void UpdateProgressBar(int i)
@@ -48,6 +58,24 @@ namespace MagicBall
                 progressBar1.Value = i + 1;
             }
             progressBar1.Value = i;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Text = APP_Name;
+
+            try
+            {
+                var date = File.ReadAllText(PREDICTIONS_CONFIG_PATH);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+
+            }
         }
     }
 }
